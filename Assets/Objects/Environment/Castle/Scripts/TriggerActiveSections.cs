1dd9715a312005d3ba55337b35e8075b6fcf36ca
Chris Julian Zaharia <chris.zaharia@gmail.com>
2014-04-20 14:52:08 +1000

    #1 Trigger sections script to load/unload gameobject areas. Section active trigger added at castle entrance

---
+++ Assets/Objects/Environment/Castle/Scripts/TriggerActiveSections.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerActiveSections : MonoBehaviour {
+	public GameObject[] loadSections;
+	public GameObject[] unloadSections;
+
+	// Load or unload areas in scene when player enters trigger
+	protected void OnTriggerEnter () {
+		if (loadSections.Length > 0) {
+			TriggerSectionActiveness(loadSections, true);
+		}
+
+		if (unloadSections.Length > 0) {
+			TriggerSectionActiveness(unloadSections, false);
+		}
+	}
+
+	private void TriggerSectionActiveness(GameObject[] sections, bool isActive) {
+		foreach (GameObject section in sections) {
+			section.SetActive (isActive);
+		}		
+	}
+}
