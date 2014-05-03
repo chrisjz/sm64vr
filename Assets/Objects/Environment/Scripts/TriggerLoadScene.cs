acf699c4b96a6e589037833343d4f907294ca372
Chris Julian Zaharia <chris.zaharia@gmail.com>
2014-05-03 18:54:44 +1000

    #2 #9 Player can enter first world from castle. Player can better walk up slopes.

---
+++ Assets/Objects/Environment/Scripts/TriggerLoadScene.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerLoadScene : MonoBehaviour {
+	public string sceneName;
+
+	protected void OnTriggerEnter () {
+		Debug.Log (1);
+		if (sceneName != null) {
+			Debug.Log (2);
+			Application.LoadLevel(sceneName);
+		}
+	}
+}
