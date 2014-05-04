20517df72d1c18e250a4bb7652271ab5a5050f9c
Chris Julian Zaharia <chris.zaharia@gmail.com>
2014-05-04 21:42:56 +1000

    #3 #18 Improved lighting in first floor of castle. Coin object created with rotation script.

---
+++ Assets/Objects/Scripts/RotateObject.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateObject : MonoBehaviour {
+	public float speed;
+
+	void Update () {
+		transform.Rotate(0, 0, Time.deltaTime * speed);
+	}
+}
