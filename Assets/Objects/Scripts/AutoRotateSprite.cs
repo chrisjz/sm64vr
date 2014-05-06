3879ab7fb6a106f4cee4de84553bee36808d52f6
Chris Julian Zaharia <chris.zaharia@gmail.com>
2014-05-07 00:44:41 +1000

    #2 Added trees, signs to castle outside area. Capitalised all gameobject names in castle scene.

---
+++ Assets/Objects/Scripts/AutoRotateSprite.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoRotateSprite : MonoBehaviour {
+	public GameObject player;
+	private Vector3 rotationVector;
+
+	void Awake () {
+		rotationVector = this.transform.rotation.eulerAngles;
+	}
+
+	// Rotate object to always face same direction as player.
+	void Update () {
+		if (player) {
+			rotationVector.y = player.transform.rotation.eulerAngles.y;
+			this.transform.rotation = Quaternion.Euler(rotationVector);
+		}
+	}
+}
