47260cd8b77ab69a41c35c879ffd0dc4f5436f39
Chris Julian Zaharia <chris.zaharia@gmail.com>
2014-05-11 16:53:53 +1000

    #2 #19 Colliders for fences in castle area. Pink bobomb always faces player.

--- Assets/Objects/Scripts/AutoRotateSprite.cs
+++ Assets/Objects/Scripts/AutoRotateSprite.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class AutoRotateSprite : MonoBehaviour {
 	public GameObject player;
+	public float offsetY = 0;
 	private Vector3 rotationVector;
 
 	void Awake () {
@@ -12,7 +13,7 @@ public class AutoRotateSprite : MonoBehaviour {
 	// Rotate object to always face same direction as player.
 	void Update () {
 		if (player) {
-			rotationVector.y = player.transform.rotation.eulerAngles.y;
+			rotationVector.y = player.transform.rotation.eulerAngles.y + offsetY;
 			this.transform.rotation = Quaternion.Euler(rotationVector);
 		}
 	}
