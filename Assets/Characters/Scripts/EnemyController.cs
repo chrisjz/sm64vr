3a8e47a60df8883c3c7b2595f5657cfbc8947b05
Chris Julian Zaharia <chris.zaharia@gmail.com>
2014-05-11 23:18:47 +1000

    #20 Enemy follows player if in view.

---
+++ Assets/Characters/Scripts/EnemyController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+	public float visionDistance = 15;
+	private NavMeshAgent agent;
+	private GameObject player;
+	private RaycastHit hit;
+	private bool followPlayer = false;
+
+	void Awake() {
+		agent = this.GetComponent<NavMeshAgent> ();
+		player = GameObject.FindWithTag("Player");
+	}
+
+	void Update () {
+		if (player) {
+			if (Physics.Raycast(transform.position, this.transform.forward, out hit)) {
+				if (hit.transform.tag == "Player" && hit.distance <= visionDistance ) {
+					followPlayer = true;
+				}
+			}
+
+			if (followPlayer) {
+				FollowPlayer();
+			}
+		}
+	}
+
+	private void FollowPlayer () {
+		agent.SetDestination (player.transform.position);
+	}
+}
