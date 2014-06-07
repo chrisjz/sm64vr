11e61c6214e0aaf539db55e60b8c925bbb85bb34
Chris Julian Zaharia <chris.zaharia@gmail.com>
2014-06-07 22:04:07 +1000

    #24 Boulder knocks back player.

---
+++ Assets/Objects/Scripts/KnockbackPlayer.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KnockbackPlayer : MonoBehaviour {
+	public int playerDamage = 1;			// Amount of damage player receives when hit by enemy
+	public float knockbackVelocity = 30;	// Distance of how much a victim is knocked back on collission with enemy
+
+	protected GameObject player;
+	protected PlayerHealth playerHealth;
+
+	void Awake () {		
+		player = GameObject.FindWithTag("Player");
+		playerHealth = player.GetComponent<PlayerHealth> ();
+	}
+	
+	protected void OnCollisionEnter(Collision col) {
+		if (!player && !playerHealth) {
+			return;
+		}
+
+		if (col.gameObject.name == player.name) {
+			Knockback(this.gameObject, player);
+		}
+	}
+	
+	protected virtual void Knockback(GameObject knocker, GameObject victim) {
+		Vector3 dir = (victim.transform.position - knocker.transform.position).normalized;
+		dir.y = 0;
+
+		CharacterMotor charMotor = player.GetComponent<CharacterMotor> ();
+		charMotor.SetVelocity (dir * knockbackVelocity);
+		playerHealth.Damage(playerDamage);
+	}
+}
