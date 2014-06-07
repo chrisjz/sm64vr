1e902032384bb3aecd4ec33778d81f2bba4ccb9a
Chris Julian Zaharia <chris.zaharia@gmail.com>
2014-06-07 18:07:11 +1000

    #24 See-saw effect for bridge in Bob-omb Battlefield.

---
+++ Assets/Characters/Scripts/PushRigidbody.cs
@@ -0,0 +1,31 @@
+﻿// Credit: titegtnodI @ Unity3D Answers
+
+using UnityEngine;
+using System.Collections;
+
+public class PushRigidbody : MonoBehaviour {
+	public float pushPower = 2.0f;
+	public float weight = 2.0f;
+
+	protected void OnControllerColliderHit (ControllerColliderHit hit) {
+		Rigidbody body = hit.collider.attachedRigidbody;
+		CharacterMotor motor = GetComponent<CharacterMotor> ();
+		Vector3 force;
+		
+		// no rigidbody
+		if (body == null || body.isKinematic) {
+			return;
+		}
+
+		// We use gravity and weight to push things down, we use
+		// our velocity and push power to push things other directions
+		if (hit.moveDirection.y < -0.3) {
+			force = new Vector3 (0f, -0.5f, 0f) * weight;
+		} else {
+			force = hit.controller.velocity * pushPower;
+		}
+
+		// Apply the push
+		body.AddForceAtPosition(force, hit.point);
+	}
+}
