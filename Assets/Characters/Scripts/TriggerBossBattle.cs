e48f409a7078252a91673fc33733c7b87878fca5
Chris Julian Zaharia <chris.zaharia@gmail.com>
2014-06-09 00:03:28 +1000

    #25 Play boss music when near King Bobomb. Wave animation for King Bobomb.

---
+++ Assets/Characters/Scripts/TriggerBossBattle.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerBossBattle : MonoBehaviour {
+	public GameObject boss;
+	public GameObject worldTheme;
+	public AudioClip bossAudioClip;
+
+	private bool bossAudioPlaying;
+
+	void Start () {
+		bossAudioPlaying = false;
+	}
+
+	void OnTriggerEnter (Collider col) {
+		if (col.tag == "Player" && !bossAudioPlaying) {
+			worldTheme.audio.clip = bossAudioClip;
+			worldTheme.audio.Play ();
+			bossAudioPlaying = true;
+		}
+	}
+
+	protected void SetAudioPlaying(bool state) {
+		bossAudioPlaying = state;
+	}
+}
