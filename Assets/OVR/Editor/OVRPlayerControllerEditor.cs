4ff869459b99d0bd04f6372e3e46aa50a9c4543b
Chris Julian Zaharia <chris.zaharia@gmail.com>
2014-08-10 19:46:38 +1000

    #27 Oculus Rift DK2 integration.

--- Assets/OVR/Editor/OVRPlayerControllerEditor.cs
+++ Assets/OVR/Editor/OVRPlayerControllerEditor.cs
@@ -6,16 +6,16 @@ Content     :   Player controller interface.
 Created     :   January 17, 2013
 Authors     :   Peter Giokaris
 
-Copyright   :   Copyright 2013 Oculus VR, Inc. All Rights reserved.
+Copyright   :   Copyright 2014 Oculus VR, Inc. All Rights reserved.
 
-Licensed under the Oculus VR SDK License Version 2.0 (the "License"); 
-you may not use the Oculus VR SDK except in compliance with the License, 
+Licensed under the Oculus VR Rift SDK License Version 3.1 (the "License"); 
+you may not use the Oculus VR Rift SDK except in compliance with the License, 
 which is provided at the time of installation or download, or which 
 otherwise accompanies this software in either electronic or hard copy form.
 
 You may obtain a copy of the License at
 
-http://www.oculusvr.com/licenses/LICENSE-2.0 
+http://www.oculusvr.com/licenses/LICENSE-3.1 
 
 Unless required by applicable law or agreed to in writing, the Oculus VR SDK 
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -57,30 +57,27 @@ public class OVRPlayerControllerEditor : Editor
 	{
 		GUI.color = Color.white;
 		
-		Undo.SetSnapshotTarget(m_Component, "OVRPlayerController");
-		
+		Undo.RecordObject(m_Component, "OVRPlayerController");
+
 		{
 			m_Component.Acceleration 	  = EditorGUILayout.Slider("Acceleration", 			m_Component.Acceleration, 	  0, 1);
 			m_Component.Damping 		  = EditorGUILayout.Slider("Damping", 				m_Component.Damping, 		  0, 1);
 			m_Component.BackAndSideDampen = EditorGUILayout.Slider("Back and Side Dampen", 	m_Component.BackAndSideDampen,0, 1);
 //			m_Component.JumpForce 		  = EditorGUILayout.Slider("Jump Force", 			m_Component.JumpForce, 		  0, 10);
 			m_Component.RotationAmount 	  = EditorGUILayout.Slider("Rotation Amount", 		m_Component.RotationAmount,   0, 5);
+			m_Component.RotationRatchet   = EditorGUILayout.Slider("Rotation Ratchet", 		m_Component.RotationRatchet,  0, 180);
 				
 			OVREditorGUIUtility.Separator();
 
-			m_Component.GravityModifier = EditorGUILayout.Slider("Gravity Modifier", m_Component.GravityModifier, 0, 1);
+			m_Component.GravityModifier = EditorGUILayout.Slider("Gravity Modifier", 		m_Component.GravityModifier, 0, 1);
 
 			OVREditorGUIUtility.Separator();
 		}
 		
 		if (GUI.changed)
 		{
-			Undo.CreateSnapshot();
-			Undo.RegisterSnapshot();
 			EditorUtility.SetDirty(m_Component);
 		}
-		
-		Undo.ClearSnapshotTarget();
 	}		
 }
 
