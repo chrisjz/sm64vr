bd5e2f80fb79d469d55ace0f1864442185f53440
Chris Julian Zaharia <chris.zaharia@gmail.com>
2014-08-16 23:36:39 +1000

    #5 Big block in bob-omb battlefield.

---
+++ Assets/Objects/Scripts/PunchObject.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PunchObject : MonoBehaviour {
+	public string emissionName = "Particles";		// Name of object that emits particles on object punch
+	public AudioClip hitAudioClip;
+
+	protected ParticleSystem emission;
+	protected bool exploding;
+
+	void Awake () {
+		Transform particleEmitter = transform.Find (emissionName);
+
+		if (particleEmitter) {
+			emission = particleEmitter.GetComponent<ParticleSystem>();
+		}
+	}
+
+	void Start () {
+		exploding = false;
+		transform.renderer.enabled = true;
+		gameObject.SetActive (true);
+	}
+	
+	void OnCollisionEnter(Collision col) {
+		if (col.gameObject.name == "LeftHandCollider" ||
+		           col.gameObject.name == "RightHandCollider" &&
+		    		!exploding) {
+			Explode();
+		}
+	}
+
+	void Explode() {
+		exploding = true;
+
+		if (!audio.isPlaying) {
+			audio.clip = hitAudioClip;
+			audio.Play();
+		}
+
+		emission.Play ();
+		
+		transform.renderer.enabled = false;
+
+		StartCoroutine (Destroy ());
+
+	}
+
+	protected IEnumerator Destroy () {
+		yield return new WaitForSeconds(5);
+		gameObject.SetActive (false);
+	}
+}
