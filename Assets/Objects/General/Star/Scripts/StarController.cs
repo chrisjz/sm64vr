f10804505aa9cd23df6b779f1897d322df7ed7f7
Chris Julian Zaharia <chris.zaharia@gmail.com>
2014-08-24 01:57:21 +1000

    #25 Player can grab star and return to castle.

--- Assets/Objects/General/Star/Scripts/StarController.cs
+++ Assets/Objects/General/Star/Scripts/StarController.cs
@@ -11,14 +11,23 @@ using UnityEngine;
 using System.Collections;
 
 public class StarController : MonoBehaviour {
+    public string worldSceneName = "Castle";                        // Scene where user goes to after grabbing star
     public AudioClip spawnAudioClip;
+    public AudioClip captureAudioClip;
     public Vector3 finalPointOffset = new Vector3 (0, 1.5f, 0);
     public float speed = 3f;
 
+    protected GameObject worldTheme;
+    protected string defaultTag;
     protected bool starMovingToFinalPoint;
+    protected bool grabbedStar;
 
     protected virtual void Awake () {
+        worldTheme = GameObject.Find ("Theme");
         starMovingToFinalPoint = false;
+        grabbedStar = false;
+        defaultTag = tag;
+        tag = "Untagged";
     }
 
     protected virtual void Update () {
@@ -27,21 +36,39 @@ public class StarController : MonoBehaviour {
             transform.position = Vector3.MoveTowards(transform.position, transform.position + finalPointOffset, step);
         }
     }
+    
+    protected void OnCollisionEnter(Collision col) {
+        Debug.Log (col.gameObject.name);
+        if (col.gameObject.name == "RightHandCollider" || col.gameObject.name == "LeftHandCollider") {
+            if (!grabbedStar) {
+                StartCoroutine(GrabStar ());
+            }
+            grabbedStar = true;
+        }
+    }
 
     public void Spawn () {
         StartCoroutine(PlaySpawnAudio ());
         StartCoroutine (MoveToFinalPoint ());
     }
+    
+    protected IEnumerator GrabStar() {
+        worldTheme.audio.clip = captureAudioClip;
+        worldTheme.audio.Play ();
+        yield return new WaitForSeconds(captureAudioClip.length);
+        Application.LoadLevel(worldSceneName);
+        
+    }
 
-    protected IEnumerator PlaySpawnAudio () {        
+    protected IEnumerator PlaySpawnAudio () {
         audio.clip = spawnAudioClip;
         audio.Play();
         starMovingToFinalPoint = true;
         yield return new WaitForSeconds(spawnAudioClip.length);
-        GameObject worldTheme = GameObject.Find ("Theme");
         SceneManager sceneManager = (SceneManager) FindObjectOfType(typeof(SceneManager));
         worldTheme.audio.clip = sceneManager.sceneAudioClip;
         worldTheme.audio.Play ();
+        tag = defaultTag;
     }
 
     protected IEnumerator MoveToFinalPoint() {
