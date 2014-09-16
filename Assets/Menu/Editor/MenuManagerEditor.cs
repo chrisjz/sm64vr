96bcbe56de9e24b9dd2aec11379251fe41ff01e5
Chris Julian Zaharia <chris.zaharia@gmail.com>
2014-09-17 00:30:08 +1000

    #30 Title menu navigation.

---
+++ Assets/Menu/Editor/MenuManagerEditor.cs
@@ -0,0 +1,41 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(MenuManager)), CanEditMultipleObjects]
+public class MenuManagerEditor : Editor {
+    
+    public SerializedProperty 
+        type_Prop,
+        action_Prop,
+        currentPanelObject_Prop,
+        nextPanelObject_Prop;
+    
+    void OnEnable () {
+        // Setup the SerializedProperties
+        type_Prop = serializedObject.FindProperty ("type");
+        action_Prop = serializedObject.FindProperty ("action");  
+        currentPanelObject_Prop = serializedObject.FindProperty("currentPanelObject");
+        nextPanelObject_Prop = serializedObject.FindProperty ("nextPanelObject");  
+    }
+    
+    public override void OnInspectorGUI() {
+        serializedObject.Update ();
+        
+        EditorGUILayout.PropertyField( type_Prop );
+        
+        MenuManager.Type st = (MenuManager.Type)type_Prop.enumValueIndex;
+        
+        switch( st ) {
+        case MenuManager.Type.Action:
+            EditorGUILayout.PropertyField( action_Prop, new GUIContent("Action") );   
+            break;            
+        case MenuManager.Type.Switcher:           
+            EditorGUILayout.PropertyField( currentPanelObject_Prop, new GUIContent("Current Panel Object") );    
+            EditorGUILayout.PropertyField ( nextPanelObject_Prop, new GUIContent("Next Panel Object") );
+            break;            
+        }
+        
+        
+        serializedObject.ApplyModifiedProperties ();
+    }
+}
---
+++ Assets/Menu/Scripts/MenuManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[AddComponentMenu("NGUI/Interaction/Menu Manager")]
+public class MenuManager : MonoBehaviour {
+    public enum Type {Action, Switcher}
+    public enum Action {Exit}
+    public Type type;
+    public Action action;
+    public GameObject currentPanelObject;
+    public GameObject nextPanelObject;
+    
+    void OnClick() {
+        switch (type) {
+            case Type.Action:
+                UpdateAction ();
+                break;
+            case Type.Switcher:
+                Switch();
+                break;
+        }
+    }
+
+    protected void UpdateAction() {
+        switch (action) {
+            case Action.Exit:
+                Application.Quit();
+                break;
+        }
+    }
+
+    protected void Switch () {
+        NGUITools.SetActive(currentPanelObject, false);
+        NGUITools.SetActive(nextPanelObject, true);
+    }
+}
